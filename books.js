bbddce8624e07978728bfbdafe95136bdedf9b3c
Arkaraj <arkaraj2017@gmail.com>
2020-12-31 18:38:10 +0530

    Added mysql database 🎉

---
+++ books.js
@@ -0,0 +1,44 @@
+const Books = [
+    {
+        id: 1,
+        name: "Book 1",
+        author: "DEF",
+        dateOfPublish: "16/3/1998",
+        price: 125,
+        description: "This book is about Geography and maps."
+    },
+    {
+        id: 2,
+        name: "Book 2",
+        author: "GHI",
+        dateOfPublish: "2/4/2002",
+        price: 500,
+        description: "This book is cursed!"
+    },
+    {
+        id: 3,
+        name: "Book 3",
+        author: "JKL",
+        dateOfPublish: "11/11/1966",
+        price: 88,
+        description: "This book is about flora and fauna."
+    },
+    {
+        id: 4,
+        name: "Book 4",
+        author: "MNO",
+        dateOfPublish: "9/12/1933",
+        price: 50,
+        description: "This book is about Space."
+    },
+    {
+        id: 5,
+        name: "Book 5",
+        author: "PQR",
+        dateOfPublish: "7/11/2010",
+        price: 155,
+        description: "This book is a story book."
+    }
+];
+
+module.exports = Books;
